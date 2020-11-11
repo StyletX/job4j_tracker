3361b38173a78334023453659e1d48841f867d64
StyletX <styletx@gmail.com>
2020-11-11 19:04:12 +0400

    1.6. Взаимодействие объектов [#382409] Заменил метод на более логичный.

--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -6,6 +6,7 @@ public class BallStory {
         Hare hare = new Hare();
         Wolf wolf = new Wolf();
         Fox fox = new Fox();
+        ball.tryGo(ball);
         hare.tryEat(ball);
         wolf.tryEat(ball);
         fox.tryEat(ball);
